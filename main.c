932ebd26bc1cf25ed435c19f2232580b6bfbdaa3
Hyeri Park <phr66@naver.com>
2023-11-16 15:42:38 +0900

    03

--- main.c
+++ main.c
@@ -1,15 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//call by reference
+void swap (int *x, int *y){
+  int temp;
+  temp = *x;
+  *x = *y;
+  *y = temp;
+}
+
 int main(int argc, char *argv[])
 {
-  int i = 300;
+  int a = 3;
+  int b = 5;
   
-  int *pi = &i;
-  char *pc = &i;
+  swap(&a,&b);
 
-  printf("%i, %i, %i\n", i, *pi, *pc);
+  printf("a:%i, b:%i\n", a, b);
 
   system("PAUSE");	
   return 0;
 }
+
+/*
+//call by value
+void swap (int x, int y){
+  int temp;
+  temp = x;
+  x = y;
+  y = temp;
+}
+
+int main(int argc, char *argv[])
+{
+  int a = 3;
+  int b = 5;
+  
+  swap(a,b);
+
+  printf("a:%i, b:%i\n", a, b);
+
+  system("PAUSE");	
+  return 0;
+}*/
